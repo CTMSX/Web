5dd50342c3a5d142189f22f50f58bd7d15027dc7
wiktorekx <wiktorr684@gmail.com>
2022-11-18 22:13:29 +0100

    Change inital

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const router = require('./routers')
+
+module.exports = function() {
+    const app = express()
+
+    app.set('views', path.join(__dirname, '..', 'views'))
+    
+    app.use(express.static(path.join(__dirname, '..', 'public')))
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    
+    app.use(cookieParser())
+
+    app.use(router())
+
+    app.listen(8080, () => {
+        console.log('App listen on 8080')
+    })
+}
